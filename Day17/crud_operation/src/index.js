2461f23700e6f30dcfb3e689b08b0dea13f8b4f7
Ajaykadu <kaduajay1122@gmail.com>
2024-12-13 23:14:06 +0530

    All Modification

---
+++ Day17/crud_operation/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import PropsDemo from './Component/propsDemo';
+import UseStateDemo from './Component/useState';
+import Parent from './Component/Parent';
+import Dashboard from './Component/Dashboard';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(<PropsDemo />);
+//root.render(<UseStateDemo />)
+//root.render(<Parent />)
+root.render(<Dashboard />)
+
