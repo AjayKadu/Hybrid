d10ac6900825cf1c9a8a24a0ff439d76192d3404
Ajaykadu <kaduajay1122@gmail.com>
2024-12-13 23:10:29 +0530

    Simple props demo

---
+++ Day17/crud_operation/src/Component/messageFun.js
@@ -0,0 +1,9 @@
+function MessageFun(props) {
+    return ( <>
+         
+         <h1>{props.myMessage} {props.day}</h1>
+    
+    </> );
+}
+
+export default MessageFun;
---
+++ Day17/crud_operation/src/Component/propsDemo.js
@@ -0,0 +1,14 @@
+import MessageFun from "./messageFun";
+
+function PropsDemo() {
+
+    return (<>
+    
+        <h1>Hello From Props...</h1>
+        <MessageFun myMessage="Hello from the Message Functional Component" day="Day17"/>
+
+
+            </>  );
+}
+
+export default PropsDemo;
