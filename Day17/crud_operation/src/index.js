9b1d8008db606caab102f69709a7f4dc2066d5dd
Ajaykadu <kaduajay1122@gmail.com>
2024-12-14 17:01:11 +0530

    CRUD Operation React with axios And ExpressJs

--- Day17/crud_operation/src/index.js
+++ Day17/crud_operation/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import DashboardWithAxio from './Component/DashboardWithAxios';
 //import PropsDemo from './Component/propsDemo';
-import UseStateDemo from './Component/useState';
-import Parent from './Component/Parent';
-import Dashboard from './Component/Dashboard';
+//import UseStateDemo from './Component/useState';
+//import Parent from './Component/Parent';
+//import Dashboard from './Component/Dashboard';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 //root.render(<PropsDemo />);
 //root.render(<UseStateDemo />)
 //root.render(<Parent />)
-root.render(<Dashboard />)
+root.render(<DashboardWithAxio />)
 
